86d6f7be70423de24d852d7eb6a3c558ae833415
Isaac Camero <icamero@proton.me>
2025-04-15 06:58:14 -0600

    website corregido estilos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  site: 'https://palabradevidausa.com', // Reemplaza con tu dominio real
+  build: {
+    assets: '_assets',
+  },
+  server: {
+    port: 3000,
+  },
+  markdown: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
